f9392912eec0edac1ddfe3be19d964cb958f58ed
Jose Eduardo Taveras Guzman <eduardotaveras1988@gmail.com>
2026-02-19 22:03:38 -0500

    Un nuevo cambio

--- Beatles.py
+++ Beatles.py
@@ -18,3 +18,5 @@ banda.insert(0, "Ringo Starr")
 print(f"\nPaso 5: El grupo finalmente es: {banda}")
 
 print(f"\nPaso 6: La cantidad de miembros es:  {len(banda)}")
+
+print(f"\nPaso 7: Este es un cambio para la tarea de las ramas")
